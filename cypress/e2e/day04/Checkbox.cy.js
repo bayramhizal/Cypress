e8a1bb75d1f12d83edc3134ca67b67a7df8fcdca
bayram <bayramhizal41@icloud.com>
2022-12-26 17:35:37 +0100

    checkbox3

--- cypress/e2e/day04/Checkbox.cy.js
+++ cypress/e2e/day04/Checkbox.cy.js
@@ -1,3 +1,5 @@
+
+
 /// reference types="cypress" />
 describe('Checkboxes', () => {
   it.skip('Single Text Box', () => {
@@ -24,7 +26,7 @@ describe('Checkboxes', () => {
       ':nth-child(21) > .MuiCollapse-wrapper > .MuiCollapse-wrapperInner > .css-7fa9du > .MuiFormControl-root > .MuiFormGroup-root > :nth-child(1) > .MuiButtonBase-root > .PrivateSwitchBase-input'
     ).uncheck();
   });
-  it('All Checkboxes', () => {
+  it.skip('All Checkboxes', () => {
     cy.visit('https://tutti.ch/');
     cy.get('#onetrust-accept-btn-handler').click();
     cy.get(
@@ -35,4 +37,29 @@ describe('Checkboxes', () => {
     cy.wait(4000);
     cy.get("input[type='checkbox']").uncheck();
   });
+    it.skip('All checkbox 2',()=>{
+            cy.visit('https://tutti.ch/');
+    cy.get('#onetrust-accept-btn-handler').click();
+     cy.get(
+      '[href="/q/autos/Ak8CkY2Fyc5TAwMDA"] > .MuiBox-root > .MuiTypography-root'
+    ).click();
+       cy.get('[aria-label="Getriebeart"] > .MuiTypography-root').scrollIntoView();
+
+       //click first checkbox
+       cy.get('.css-uf9pwq > .MuiFormControlLabel-root > .MuiButtonBase-root > .PrivateSwitchBase-input').eq(0).check()
+       //click 10.th cehckbox
+      cy.get("input[type='checkbox']").eq(4).check().should('be.checked')
+
+    })
+      it('All Checkbox 3',()=>{
+            cy.visit('https://tutti.ch/');
+    cy.get('#onetrust-accept-btn-handler').click();
+     cy.get(
+      '[href="/q/autos/Ak8CkY2Fyc5TAwMDA"] > .MuiBox-root > .MuiTypography-root'
+    ).click();
+       cy.get('[aria-label="Getriebeart"] > .MuiTypography-root').scrollIntoView();
+       cy.get("input[type='checkbox']").click({multiple: true})
+
+        
+    })
 });
